6bfb05f563fe3c65c89da53f71059f39ecb64d83
Brenner Hernández <velasquezmochis@gmail.com>
2019-04-03 13:23:47 -0600

    Creation of main view

---
+++ ProjectSQL/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Optimization;
+
+namespace ProjectSQL {
+
+    public class BundleConfig {
+
+        public static void RegisterBundles(BundleCollection bundles) {
+            // JS
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                "~/Scripts/site.js"));
+
+            //CSS
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                 "~/Content/menu.css",
+                 "~/Content/site.css"));
+        }
+
+    }
+
+}
