0272941b9dbbc7cca882b14bcce7841f418328a8
Brenner Hernández <velasquezmochis@gmail.com>
2019-04-03 21:50:33 -0600

    Corrected spelling

--- ProjectSQL/App_Start/RouteConfig.cs
+++ ProjectSQL/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace ProjectSQL
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "TechDB", action = "SQLCode", id = UrlParameter.Optional }
             );
         }
     }
