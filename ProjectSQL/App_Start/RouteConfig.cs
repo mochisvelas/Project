4513ff8979eb01e12687bc972c7654b2b5673b4d
Roberto Solares <betosolaresgar@gmail.com>
2019-04-23 15:48:50 -0700

    Change the route configuration

--- ProjectSQL/App_Start/RouteConfig.cs
+++ ProjectSQL/App_Start/RouteConfig.cs
@@ -1,23 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace ProjectSQL
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+namespace ProjectSQL {
 
+    public class RouteConfig {
+
+        public static void RegisterRoutes(RouteCollection routes) {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "TechDB", action = "SQLCode", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "DBMS", action = "SQLCode", id = UrlParameter.Optional });
         }
+
     }
+
 }
