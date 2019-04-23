4961b0fdbea9e6430b2573f243b0338f2a041950
Roberto Solares <betosolaresgar@gmail.com>
2019-04-23 15:54:25 -0700

    Create and add the bundle configuration

---
+++ ProjectSQL/App_Start/BundleConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Optimization;
+
+namespace ProjectSQL {
+
+    public class BundleConfig {
+
+        public static void RegisterBundles(BundleCollection bundles) {
+            // CSS Bundle
+            bundles.Add(new StyleBundle("~/bundles/style").Include(
+                                        "~/Styles/bulma.min.css"));
+        }
+
+    } 
+
+}
