54fe2683dd12aeb0db49b0b67679f549c4f79288
Roberto Solares <betosolaresgar@gmail.com>
2019-04-27 09:45:01 -0700

    Create the default dictionarie for the reserved words

--- ProjectSQL/Controllers/DBMSController.cs
+++ ProjectSQL/Controllers/DBMSController.cs
@@ -1,9 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace ProjectSQL.Controllers {
 
     public class DBMSController : Controller {
-        
+
+        // Reserved words dictionarie
+        private static Dictionary<string, List<string>> reservedWords = new Dictionary<string, List<string>>() {
+            { "SELECT", new List<string>(){ "SELECT" } },
+            { "FROM", new List<string>(){ "FROM" } },
+            { "DELETE", new List<string>(){ "DELETE" } },
+            { "WHERE", new List<string>(){ "WHERE" } },
+            { "CREATE TABLE", new List<string>(){ "CREATE TABLE" } },
+            { "DROP TABLE", new List<string>(){ "DROP TABLE" } },
+            { "INSERT INTO", new List<string>(){ "INSERT INTO" } },
+            { "VALUES", new List<string>(){ "VALUES" } },
+            { "GO", new List<string>(){ "GO" } }
+        };
+
         // Return the view to load the reserved words
         [HttpGet]
         public ActionResult LoadReservedWords() {
