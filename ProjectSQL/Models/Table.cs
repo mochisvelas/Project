9dc98b09c04128b49929764d1cdbdbb4c425bd57
Roberto Solares <betosolaresgar@gmail.com>
2019-05-09 18:27:20 -0700

    Add the table model

---
+++ ProjectSQL/Models/Table.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace ProjectSQL.Models {
+
+    public class Table {
+
+        public string name { get; set; }
+        public List<KeyValuePair<string, string>> columns { get; set; }
+
+    }
+
+}
